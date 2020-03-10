613e4ed9158bb13e18e0f71a1c87b00992a03061
Michael <mmbmf.mm@gmail.com>
2020-03-10 10:08:16 -0500

    completed up to drill 11

--- drills5_12.js
+++ drills5_12.js
@@ -50,25 +50,69 @@ const maxSum = arr => {
   return newSum;
 };
 
-console.log(maxSum([4, 6, -3, 5, -2, 1]));
-
 // Input: [4, 6, -3, 5, -2, 1]
 // Output: 12
+
+console.log(maxSum([4, 6, -3, 5, -2, 1]));
+
 // 8. Merge arrays
 // Imagine you have 2 arrays which have already been sorted. Write an algorithm to merge the 2 arrays into a single array, which should also be sorted.
 
 // Input:[1, 3, 6, 8, 11] and [2, 3, 5, 8, 9, 10]
 // Output:[1, 2, 3, 3, 5, 6, 8, 8, 9, 10, 11]
+
+const mergeArray = (arr1, arr2) => {
+  return [...arr1, ...arr2].sort((a, b) => a - b);
+};
+
+console.log(mergeArray([1, 3, 6, 8, 11], [2, 3, 5, 8, 9, 10]));
+
 // 9. Remove characters
 // Write an algorithm that deletes given characters from a string. For example, given a string of "Battle of the Vowels: Hawaii vs. Grozny" and the characters to be removed are "aeiou", the algorithm should transform the original string to "Bttl f th Vwls: Hw vs. Grzny". Do not use Javascript's filter, split, or join methods.
 
 // Input:'Battle of the Vowels: Hawaii vs. Grozny', 'aeiou'
 // Output: 'Bttl f th Vwls: Hw vs. Grzny'
+
+const removeChars = (str, remove) => {
+  let removeStr = "";
+  for (let i = 0; i < remove.length; i++) {
+    removeStr += remove[i] + "|";
+  }
+
+  // console.log(removeStr);
+
+  return str.replace(new RegExp(removeStr, "g"), "");
+};
+
+console.log(removeChars("Battle of the Vowels: Hawaii vs. Grozny", "aeiou"));
+
 // 10. Products
 // Given an array of numbers, write an algorithm to find out the products of every other number except the number at each index.
 
 // Input:[1, 3, 9, 4]
 // Output:[108, 36, 12, 27]
+
+const products = arr => {
+  function multiply(array) {
+    let sum = 1;
+    for (let i = 0; i < array.length; i++) {
+      sum = sum * array[i];
+    }
+    return sum;
+  }
+
+  let newArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    let product = [...arr];
+    product.splice(i, 1);
+    newArr.push(multiply(product));
+  }
+  return newArr;
+};
+
+console.log(products([1, 3, 9, 4]));
+
 // 11. 2D array
 // Write an algorithm which searches through a 2D array, and whenever it finds a 0 should set the entire row and column to 0.
 
@@ -84,6 +128,7 @@ console.log(maxSum([4, 6, -3, 5, -2, 1]));
 // [0,0,1,1,0],
 // [0,0,0,0,0],
 // [0,0,1,1,0]];
+
 // 12. String rotation
 // Given 2 strings, str1 and str2, write a program that checks if str2 is a rotation of str1.
 
